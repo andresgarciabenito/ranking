7b14296ca117f2db94a63566732efb17dc3eb6cd
andresgarciabenito <andresgarciabenito@hotmail.com>
2020-06-23 11:13:42 +0200

    2

--- script_starsII.js
+++ script_starsII.js
@@ -1,16 +1,22 @@
-var stars=document.getElementsByClassName('star');
-
-
-function on(){
-  for (var i=0; i<stars.length; i++){
-    stars[4].classList.remove('unchecked');
-    stars[4].classList.add('checked');
-  }
+function clearRates() { //primer paso limpiar todos los rates
+    for(var i=1; i<=5; i++) {
+        document.getElementById("star"+i).classList.remove("checked");
+    }
 }
 
-// function off(){
-//   for (var i=0; i<stars.length; i++){
-//       stars[i].classList.remove('checked');
-//       stars[i].classList.add('unchecked');
-// }
-// }
+function on(value){
+	clearRates();
+	for(var i=1; i<=value; i++) {
+        document.getElementById("star"+i).classList.add("checked");
+    }
+}
+
+
+body.addEventListener("click", off());
+
+function off(){
+  for (var i=0; i<5; i++){
+      stars[i].classList.remove('checked');
+      stars[i].classList.add('unchecked');
+}
+}
